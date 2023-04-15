575d23d50a4351f0373132637473791ca26ee9f9
lovez <710096382@qq.com>
2023-04-15 12:53:23 +0000

    update util/baseConfig.js.
    Signed-off-by: lovez <710096382@qq.com>

--- util/baseConfig.js
+++ util/baseConfig.js
@@ -2,9 +2,6 @@
 
 export default {
 	//后端接口
-	baseUrl: 'https://api.bot.hongchiqingyun.com',
-	baseWsUrl: "wss://api.bot.hongchiqingyun.com/websocket/chat",
-	//后端接口
-	// baseUrl: 'http://127.0.0.1:8081',
-	// baseWsUrl: "ws://127.0.0.1:8081/websocket/chat"
+	baseUrl: 'http://127.0.0.1:8081',
+	baseWsUrl: "ws://127.0.0.1:8081/websocket/chat"
 }
